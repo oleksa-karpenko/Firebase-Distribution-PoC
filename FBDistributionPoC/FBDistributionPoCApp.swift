a5636e7c7d7eca4bbe26722f512943426ec9a39a
Oleksandr Karpenko <okarp@softserveinc.com>
2025-06-12 08:48:50 +0300

    Initial Commit

---
+++ FBDistributionPoC/FBDistributionPoCApp.swift
@@ -0,0 +1,17 @@
+//
+//  FBDistributionPoCApp.swift
+//  FBDistributionPoC
+//
+//  Created by Oleksandr Karpenko on 12.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FBDistributionPoCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
